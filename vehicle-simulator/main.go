1edf18638b15058d48210c40c7f6996d53d00e66
Dimitar Dimitrov <dimitar.dimitrov3@bosch.com>
2023-11-16 09:54:23 +0200

    test4

--- vehicle-simulator/main.go
+++ vehicle-simulator/main.go
@@ -58,7 +58,7 @@ func main() {
 	keepAlive := make(chan os.Signal, 1)
 	signal.Notify(keepAlive, os.Interrupt, syscall.SIGTERM)
 
-	   initializeClient()
+	initializeClient()
 	getDeviceData()
 
 	defer func() {
